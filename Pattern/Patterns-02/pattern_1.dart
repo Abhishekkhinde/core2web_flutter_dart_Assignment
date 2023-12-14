a3715e7a58afa926380c61b18d0e7ba9dde5b765
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-12-15 01:20:13 +0530

    pattern 1

---
+++ Pattern/Patterns-02/pattern_1.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main() {
+  print("Enter a row:");
+  int row = int.parse(stdin.readLineSync()!);
+
+  print("Enter column");
+  int col = int.parse(stdin.readLineSync()!);
+
+  int a = 1;
+
+  for (int i = 1; i <= row; i++) {
+    for (int j = 1; j <= col; j++) {
+      stdout.write("$a ");
+      a++;
+    }
+    print("");
+  }
+}
