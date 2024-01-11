f1298742eb0020f314e6f1149ee8989cc5fce830
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2024-01-11 23:23:47 +0530

    Mixin

---
+++ Mixin/program3.dart
@@ -0,0 +1,23 @@
+mixin class Parent {
+  Parent() {}
+  int x = 10;
+  static int y = 199;
+
+  void fun() {
+    print(x);
+    print(y);
+  }
+}
+
+class Child extends Parent {
+  int x = 39;
+
+  void gun() {
+    super.fun();
+  }
+}
+
+void main() {
+  Child obj = new Child();
+  obj.gun();
+}
